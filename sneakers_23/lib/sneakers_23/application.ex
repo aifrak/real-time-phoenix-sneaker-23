f70571251056d0021c9961de12d823d0d72060a5
aifrak <aifrak@users.noreply.github.com>
2022-01-06 18:30:29 +0000

    Add code from "Run Multiple Servers"

--- sneakers_23/lib/sneakers_23/application.ex
+++ sneakers_23/lib/sneakers_23/application.ex
@@ -1,11 +1,11 @@
-#---
+# ---
 # Excerpted from "Real-Time Phoenix",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/sbsockets for more book information.
-#---
+# ---
 defmodule Sneakers23.Application do
   @moduledoc false
 
@@ -15,7 +15,8 @@ defmodule Sneakers23.Application do
     children = [
       Sneakers23.Repo,
       Sneakers23Web.Endpoint,
-      Sneakers23.Inventory
+      Sneakers23.Inventory,
+      Sneakers23.Replication
     ]
 
     opts = [strategy: :one_for_one, name: Sneakers23.Supervisor]
