c340ac5ed0962f5f3bc8c926823f128e9141788f
aifrak <aifrak@users.noreply.github.com>
2022-01-10 17:55:33 +0000

    Add code from "Build Your Shopping Cart Channel" - "Synchronize Multiple Channel Clients"

--- sneakers_23/lib/sneakers_23_web/channels/shopping_cart_channel.ex
+++ sneakers_23/lib/sneakers_23_web/channels/shopping_cart_channel.ex
@@ -4,6 +4,8 @@ defmodule Sneakers23Web.ShoppingCartChannel do
 
   import Sneakers23Web.CartView, only: [cart_to_map: 1]
 
+  intercept ["cart_updated"]
+
   def join("cart:" <> id, params, socket) when byte_size(id) == 64 do
     cart = get_cart(params)
     socket = assign(socket, :cart, cart)
@@ -20,12 +22,43 @@ defmodule Sneakers23Web.ShoppingCartChannel do
   def handle_in("add_item", %{"item_id" => id}, socket = %{assigns: %{cart: cart}}) do
     case Checkout.add_item_to_cart(cart, String.to_integer(id)) do
       {:ok, new_cart} ->
+        broadcast_cart(new_cart, socket, added: [id])
         socket = assign(socket, :cart, new_cart)
         {:reply, {:ok, cart_to_map(new_cart)}, socket}
 
       {:error, :duplicate_item} ->
         {:reply, {:error, %{error: "duplicate_item"}}, socket}
     end
+  end
+
+  def handle_in("remove_item", %{"item_id" => id}, socket = %{assigns: %{cart: cart}}) do
+    case Checkout.remove_item_from_cart(cart, String.to_integer(id)) do
+      {:ok, new_cart} ->
+        broadcast_cart(new_cart, socket, removed: [id])
+        socket = assign(socket, :cart, new_cart)
+        {:reply, {:ok, cart_to_map(new_cart)}, socket}
+
+      {:error, :not_found} ->
+        {:reply, {:error, %{error: "not_found"}}, socket}
+    end
+  end
+
+  def handle_out("cart_updated", params, socket) do
+    cart = get_cart(params)
+    socket = assign(socket, :cart, cart)
+    push(socket, "cart", cart_to_map(cart))
+
+    {:noreply, socket}
+  end
+
+  defp broadcast_cart(cart, socket, opts) do
+    {:ok, serialized} = Checkout.export_cart(cart)
+
+    broadcast_from(socket, "cart_updated", %{
+      "serialized" => serialized,
+      "added" => Keyword.get(opts, :added, []),
+      "removed" => Keyword.get(opts, :removed, [])
+    })
   end
 
   defp get_cart(params) do
