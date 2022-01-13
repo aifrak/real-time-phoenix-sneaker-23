bac83ab2325ca93cf7617505c709a8c83e11d0b7
aifrak <aifrak@users.noreply.github.com>
2022-01-13 17:34:26 +0000

    Add code from "Assemble the Admin Dashboard"

--- sneakers_23/assets/js/admin.js
+++ sneakers_23/assets/js/admin.js
@@ -1,7 +1,7 @@
 import { Presence } from 'phoenix'
 import '../css/admin.css'
 import "../css/app.css"
-import './admin/dom'
+import dom from './admin/dom'
 import { adminSocket } from "./admin/socket"
 
 adminSocket.connect()
@@ -13,3 +13,41 @@ window.presence = presence // This is a helper for us
 cartTracker.join().receive("error", () => {
   console.error("Channel join failed")
 })
+
+presence.onSync(() => {
+  dom.setShopperCount(getShopperCount(presence))
+  dom.assemblePageCounts(getPageCounts(presence))
+
+  const itemCounts = getItemCounts(presence)
+  dom.resetItemCounts()
+  Object.keys(itemCounts).forEach((itemId) => {
+    dom.setItemCount(itemId, itemCounts[itemId])
+  })
+})
+function getShopperCount(presence) {
+  return Object.keys(presence.state).length
+}
+
+function getPageCounts(presence) {
+  const pageCounts = {}
+  Object.values(presence.state).forEach(({ metas }) => {
+    metas.forEach(({ page }) => {
+      pageCounts[page] = pageCounts[page] || 0
+      pageCounts[page] += 1
+    })
+  })
+  return pageCounts
+}
+
+function getItemCounts(presence) {
+  const itemCounts = {}
+  Object.values(presence.state).forEach(({ metas }) => {
+    // We only need to worry about the first metadata for each shopper,
+    // because a shopper's cart synchronizes across tabs.
+    metas[0].items.forEach((itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0
+      itemCounts[itemId] += 1
+    })
+  })
+  return itemCounts
+}
